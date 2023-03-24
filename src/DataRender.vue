791baab28b39ffa8a8c645e43284722577dfd227
Arsen <arsenehakobian@gmail.com>
2023-03-25 00:39:58 +0400

    Dynamicly render items in . Not working fully

--- src/DataRender.vue
+++ src/DataRender.vue
@@ -1,12 +1,23 @@
 <template>
-  <ul role="list" class="p-6 divide-y divide-slate-200">
-    <li class="flex py-4 first:pt-0 last:pb-0">
-      <div class="ml-3 overflow-hidden">
-        <p class="text-sm font-medium text-slate-900">{{ person.name }}</p>
-        <p class="text-sm text-slate-500 truncate">{{ person.email }}</p>
-      </div>
-    </li>
-  </ul>
+  <div class="flex flex-row">
+    <ul
+      v-for="(item, index) in $data"
+      :key="index"
+      role="list"
+      class="p-6 divide-y divide-slate-200"
+    >
+      <li
+        v-for="(value, key) in item"
+        :key="key"
+        class="flex py-4 first:pt-0 last:pb-0"
+      >
+        <div class="ml-3 overflow-hidden">
+          <p class="text-sm font-medium text-slate-900">{{ key }}</p>
+          <p class="text-sm text-slate-500 truncate">{{ value }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -15,10 +26,6 @@ import axios from "axios";
 export default {
   data() {
     return {
-      person: {
-        name: "Arsen",
-        email: "asdad@mail.ru",
-      },
       zipCodeData: null,
       userLocationData: null,
       userUAData: null,
@@ -41,7 +48,10 @@ export default {
         .get(
           `https://app.zipcodebase.com/api/v1/search?apikey=5412dd50-ca7e-11ed-8557-7349d60d7194&codes=${this.$route.params.zip}&country=us`
         )
-        .then((resp) => console.log(resp));
+        .then(
+          (resp) =>
+            (this.zipCodeData = resp.data.results[this.$route.params.zip][0])
+        );
     },
     renderUserData() {
       navigator.geolocation.getCurrentPosition(
